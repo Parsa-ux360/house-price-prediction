6981f299ff3faf3dee4c8d14f91931d618dbd521
Parsa-ux360 <parsashahi2010ir@gmail.com>
2026-06-17 15:59:50 +0330

    reduce model size for deployment

--- train_model.py
+++ train_model.py
@@ -19,7 +19,8 @@ y = df['Price']
 X_train , X_test , y_train , y_test = train_test_split(X , y , test_size= 0.2 , random_state= 42)
 
 model = RandomForestRegressor(
-    n_estimators=100,
+    n_estimators=50,
+    max_depth=15,
     random_state=42
 )
 
